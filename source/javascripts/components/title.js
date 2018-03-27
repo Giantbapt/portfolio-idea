62f30eb24eefcb0be9b5b5cd33ab3e634a472e7c
Baptiste Chebassier <baptiste.chebassier28@gmail.com>
2018-03-27 18:25:36 +0200

    wrapper

--- source/javascripts/components/title.js
+++ source/javascripts/components/title.js
@@ -2,7 +2,7 @@ import Typed from 'typed.js';
 
 function loadDynamicBannerText() {
   new Typed('#banner-typed-text', {
-    strings: ["Middleman is ready", "Webpack too"],
+    strings: ["This Portfolio is Awesome", "It's Baptiste Chebassier's one"],
     typeSpeed: 50,
     loop: true
   });
