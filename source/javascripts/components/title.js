647b3cae013f1020ec09cb05f52886a3be9d9c7f
Baptiste Chebassier <baptiste.chebassier28@gmail.com>
2018-03-27 23:22:36 +0200

    banner changed

--- source/javascripts/components/title.js
+++ source/javascripts/components/title.js
@@ -2,7 +2,7 @@ import Typed from 'typed.js';
 
 function loadDynamicBannerText() {
   new Typed('#banner-typed-text', {
-    strings: ["This Portfolio is Awesome", "It's Baptiste Chebassier's one"],
+    strings: ["This Portfolio is Awesome", "It's Baptiste Chebassier's"],
     typeSpeed: 50,
     loop: true
   });
